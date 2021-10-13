6e3c0ed2f783fed802de258253ca6a23c748c2ad
Thien Ly <thienly@NTs-MacBook-Pro.local>
2021-10-13 22:00:11 +0700

    update

--- hello.go
+++ hello.go
@@ -1,6 +1,8 @@
 package hello
 
 import (
+	"fmt"
+
 	quoteV3 "rsc.io/quote/v3"
 )
 
@@ -8,5 +10,6 @@ func Hello() string {
 	return quoteV3.HelloV3()
 }
 func Proverb() string {
+	fmt.Printf("Hello world")
 	return quoteV3.Concurrency()
 }
